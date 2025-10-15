4aa477833225dc2f6f8a5aa57c0b6f1df9ed69cb
Kristian Ivarsson <kristone@hotmail.com>
2025-10-15 04:32:45 +0200

    feature: initial stable implementation of basic data structures

---
+++ include/poly/fifo.hpp
@@ -0,0 +1,108 @@
+//
+// Copyright (c) 2025 Kristian Ivarsson
+//
+// Licensed under the MIT License. See https://opensource.org/licenses/MIT for details.
+//
+
+#pragma once
+
+#include <vector>
+#include <utility>
+#include <format>
+#include <stdexcept>
+
+namespace poly
+{
+
+   inline namespace v1_0_0
+   {
+
+      template< typename Key, typename T>
+      struct fifo : std::vector< std::pair< Key, T>>
+      {
+         using base_type = std::vector< std::pair< Key, T>>;
+
+         using base_type::base_type;
+
+         using value_type = base_type::value_type;
+         using key_type = Key;
+         using mapped_type = T;
+         using size_type = base_type::size_type;
+
+         auto find( this auto&& self, const auto& key)
+         {
+            return std::ranges::find( self, key, &value_type::first);
+         }
+
+         auto contains( const auto& key) const
+         {
+            return find( key) != base_type::end();
+         }
+
+         auto erase( const auto& key)
+         {
+            if( auto result = find( key); result != base_type::end())
+               return base_type::erase( result), size_type{ 1};
+            else
+               return size_type{ 0};
+         }
+
+         auto& at( this auto&& self, const auto& key)
+         {
+            if(auto result = self.find( key); result != self.end())
+               return result->second;
+            else
+               throw std::out_of_range{ std::format( "{}", key)};
+         }
+
+         auto& operator []( auto&& key)
+         {
+            if(auto result = find( key); result != base_type::end())
+               return result->second;
+            else
+               return base_type::emplace_back( std::forward< decltype( key)>( key), mapped_type{}).second;
+         }
+
+         auto insert( auto&& value)
+         {
+            if(auto result = find( value.first); result != base_type::end())
+               return std::make_pair( result, false);
+            else
+               return base_type::emplace_back( std::move( value)), std::make_pair( std::prev( base_type::end()), true);
+         }
+
+         auto insert_or_assign( auto&& key, auto&& object)
+         {
+            if(auto result = find( key); result != base_type::end())
+               return result->second = std::move( object), std::make_pair( result, false);
+            else
+               return base_type::emplace_back( std::move( key), std::move( object)), std::make_pair( std::prev( base_type::end()), true);
+         }
+
+         auto emplace( auto&&... parameters)
+         {
+            if(auto result = find( std::get< 0>( std::tie( parameters...))); result != base_type::end())
+               return std::make_pair( result, false);
+            else
+               return base_type::emplace_back( std::forward< decltype( parameters)>( parameters)...), std::make_pair( std::prev( base_type::end()), true);
+         }
+
+      private:
+      
+         //using base_type::append_range;
+         //using base_type::assign_range;
+         //using base_type::insert_range;
+
+         using base_type::assign;
+         using base_type::insert;
+         using base_type::emplace;
+         using base_type::emplace_back;
+         using base_type::push_back;
+         using base_type::erase;
+         using base_type::resize;
+
+      };
+
+   } // v1_0_0
+
+} // poly
---
+++ include/poly/node.hpp
@@ -0,0 +1,82 @@
+//
+// Copyright (c) 2025 Kristian Ivarsson
+//
+// Licensed under the MIT License. See https://opensource.org/licenses/MIT for details.
+//
+
+#pragma once
+
+#include <variant>
+#include <string>
+#include <vector>
+
+#include "fifo.hpp"
+
+namespace poly
+{
+   inline namespace v1_0_0
+   {
+      struct node;
+
+      using data = std::variant< std::nullptr_t, bool, long, double, std::string, std::vector< node>, fifo< std::string, node>>;
+
+      struct node : data
+      {
+         using data::data;
+         using data::operator=;
+
+         //! @{ member
+         using nothing = std::variant_alternative_t< 0, data>;
+         using boolean = std::variant_alternative_t< 1, data>;
+         using integer = std::variant_alternative_t< 2, data>;
+         using decimal = std::variant_alternative_t< 3, data>;
+         using string = std::variant_alternative_t< 4, data>;
+         using array = std::variant_alternative_t< 5, data>;
+         using table = std::variant_alternative_t< 6, data>;
+         //! @}
+
+         //! @{ access
+         template< typename type> auto& as( this auto&& self) { return std::get< type>( self); }
+
+         auto& as_nothing( this auto&& self) { return self.template as< nothing>(); }
+         auto& as_boolean( this auto&& self) { return self.template as< boolean>(); }
+         auto& as_integer( this auto&& self) { return self.template as< integer>(); }
+         auto& as_decimal( this auto&& self) { return self.template as< decimal>(); }
+         auto& as_string( this auto&& self) { return self.template as< string>(); }
+         auto& as_array( this auto&& self) { return self.template as< array>(); }
+         auto& as_table( this auto&& self) { return self.template as< table>(); }
+
+         auto& at( this auto& self, const auto& lookup) requires std::is_convertible_v< decltype( lookup), array::size_type>
+         {
+           return self.as_array().at( lookup);
+         }
+
+         auto& at( this auto& self, const auto& lookup) requires std::is_convertible_v< decltype( lookup), table::key_type>
+         {
+           return self.as_table().at( lookup);
+         }
+         //! @}
+
+         //! @{ lookup (and access)
+         template< typename type> auto to( this auto&& self) noexcept { return std::get_if< type>( &self); }
+
+         auto to_nothing( this auto&& self) noexcept { return self.template to< nothing>(); }
+         auto to_boolean( this auto&& self) noexcept { return self.template to< boolean>(); }
+         auto to_integer( this auto&& self) noexcept { return self.template to< integer>(); }
+         auto to_decimal( this auto&& self) noexcept { return self.template to< decimal>(); }
+         auto to_string( this auto&& self) noexcept { return self.template to< string>(); }
+         auto to_array( this auto&& self) noexcept { return self.template to< array>(); }
+         auto to_table( this auto&& self) noexcept { return self.template to< table>(); }
+
+         bool is_null() const noexcept { return to_nothing(); }
+         bool is_true() const noexcept { return to_boolean() && as_boolean(); }
+         bool is_false() const noexcept { return to_boolean() && not as_boolean(); }
+         bool is_scalar() const noexcept { return not is_composite(); }
+         bool is_numeric() const noexcept { return to_integer() or to_decimal(); }
+         bool is_composite() const noexcept { return to_array() or to_table(); }
+         //! @}
+      };
+
+   } // v1_0_0
+
+} // poly
